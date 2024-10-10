f6fda3a7c1e807a07e8ebe7f815d650e681e1ea1
Kaoutar Laouaj <kaoutar.laouaj@ump.ac.ma>
2024-10-10 17:41:47 +0100

    New changes in cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
@@ -6,11 +5,11 @@ import Image from 'next/image'
 import profilePic from "../../public/images/profile/laouaj-kaoutar2.png"
 import AnimatedText from '@/components/AnimtedText'
 import Link from 'next/link';
-import { useEffect } from 'react';
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg"
 import TransitionEffect from '@/components/TransitionEffect'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -22,41 +21,50 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <TransitionEffect/>
+      <TransitionEffect />
       <main className="flex items-center text-dark w-full min-h-screen">
-       <Layout className='pt-0 md:p-16 sm:pt-8'>
+        <Layout className='pt-0 md:p-16 sm:pt-8'>
           <div className="flex items-center justify-between w-full lg:flex-col">
-           <div className='w-1/2 md:w-full'>
-             <Image src={profilePic} alt="CodeBucks" className='w-full h-auto lg:hidden md:inline-block md:w-full' priority sizes="(max-width:768px) 100vw,
-             (max-width: 1200px) 50vw, 50vw"/>
-           </div>
-           <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-            <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm::text-3xl'/>
-            <p className='my-4 text-base font-medium dark:text-light md:text-sm sm:text-xs'>
-            As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-Explore my latest projects and articles, showcasing my expertise in React.js and web development.
-            </p>
-            <div className='flex items-center self-start mt-2 lg:self-center'>
-             <Link href="/CV_kaoutar_laouaj.pdf" target={"_blank"}
-             className="flex items-center bg-dark text-light p-2.5  px-2
-             rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
-             border-2 border-solid border-tranzsparent hover:border-dark
-             dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-             hover:dark:border-light md:p-2 md:px-4 md:text-base"
-             download={true}
-             >CV <LinkArrow className={"w-4 ml-1"}/>
-             </Link>
-             <Link href="mailto:kaoutar.laouaj@ump.ac.ma" target={"_blank"}
-             className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base"
-             >Contact me</Link>
-           </div>
-           </div>
+            <div className='w-1/2 md:w-full'>
+              <Image
+                src={profilePic}
+                alt="Kaoutar Laouaj"
+                className='w-full h-auto lg:hidden md:inline-block md:w-full'
+                priority
+                sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+              />
+            </div>
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'
+              />
+              <p className='my-4 text-base font-medium dark:text-light md:text-sm sm:text-xs'>
+                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+              </p>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
+                <Link href="/CV_kaoutar_laouaj.pdf" target="_blank"
+                  className="flex items-center bg-dark text-light p-2.5 px-2 rounded-lg text-lg font-semibold
+                  hover:bg-light hover:text-dark border-2 border-transparent hover:border-dark
+                  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
+                  download={true}
+                >
+                  CV <LinkArrow className="w-4 ml-1" />
+                </Link>
+                <Link href="mailto:kaoutar.laouaj@ump.ac.ma" target="_blank"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base"
+                >
+                  Contact me
+                </Link>
+              </div>
+            </div>
           </div>
-       </Layout>
-       <HireMe />
-       <div className='absolute right-0 bottom-0 inline-block w-20 md:hidden'>
-        <Image src={lightBulb} alt="Koki" className='w-full h-auto'/>
-       </div>
+        </Layout>
+        <HireMe />
+        <div className='absolute right-0 bottom-0 inline-block w-20 md:hidden'>
+          <Image src={lightBulb} alt="Light Bulb" className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
