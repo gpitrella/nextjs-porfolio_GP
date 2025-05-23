ca6487a9eb984bccb116ee2ddc768f073b034b74
gpitrella <gabriel98_@hotmail.com>
2025-05-23 09:41:09 -0300

    update analitycs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import NavBar from '@/components/NavBar';
 import '@/styles/globals.css'
-import {Montserrat} from "next/font/google"
+import { Montserrat } from "next/font/google"
 import Head from 'next/head';
 import Footer from '@/components/Footer';
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 const montserrat = Montserrat(
   {
     subsets: ["latin"],
@@ -11,12 +13,16 @@ const montserrat = Montserrat(
 )
 
 export default function App({ Component, pageProps }) {
+  
+  console.log('process.env.NEXT_PUBLIC_GA_ID: ', process.env.NEXT_PUBLIC_GA_ID)
+
   return (
  
  <>
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
       <NavBar />
@@ -24,6 +30,7 @@ export default function App({ Component, pageProps }) {
       <Footer />
     </main>
  </>
-  );
-  
+  );  
 }
+
+
