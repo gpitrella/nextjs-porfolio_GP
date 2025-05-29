2f7a6323fa8cbb7d4750a8c5c9296ab038c16dd7
gpitrella <gabriel98_@hotmail.com>
2025-05-29 09:58:58 -0300

    update tagmanager

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -3,7 +3,16 @@ import Script from "next/script";
 export default function GoogleAnalytics() {
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} strategy="afterInteractive" />
+    {/* <!-- Google tag (gtag.js) --> */}
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFZE480F9S"></Script>
+    <Script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+
+      gtag('config', 'G-ZFZE480F9S');
+    </Script>
+      {/* <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -11,7 +20,7 @@ export default function GoogleAnalytics() {
           gtag('js', new Date());
           gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
         `}
-      </Script>
+      </Script> */}
     </>
   );
 }
