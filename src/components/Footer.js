386da64839a04d7fb9066789e4e18af88aace1ec
gpitrella <gabriel98_@hotmail.com>
2025-08-27 16:55:16 -0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "./Layout";
 import Link from "next/link";
+import Button from "./Button";
+
 const Footer = () => {
     return (
         <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base">
@@ -10,7 +12,7 @@ const Footer = () => {
            Build With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span> 
            by&nbsp; <Link href="https://github.com/gpitrella" className="underline underline-offset-2 ">Gabriel Pitrella</Link>
             </div>
-
+            <Button text="Alert" />
             </Layout>
         </footer>
     )
